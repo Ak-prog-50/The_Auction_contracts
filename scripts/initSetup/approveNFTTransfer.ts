0150b847f3e85593059f9d94f73e37ca3bdbc227
Ak <akalankapathirage22@gmail.com>
2022-06-18 22:39:57 +0530

    added type declaration to auctionNFT

--- scripts/initSetup/approveNFTTransfer.ts
+++ scripts/initSetup/approveNFTTransfer.ts
@@ -1,9 +1,10 @@
 import { ethers, getNamedAccounts } from "hardhat"
+import { AuctionNFT } from "../../typechain";
 
 
 export const approveNFTTransfer = async () => {
     const { auctionHost } = await getNamedAccounts();
-    const auctionNFT = await ethers.getContract("AuctionNFT")
+    const auctionNFT: AuctionNFT = await ethers.getContract("AuctionNFT")
     const auction = await ethers.getContract("Auction")
     const auctionHostSigner = await ethers.getSigner(auctionHost)
     const approveTx = await auctionNFT.connect(auctionHostSigner).approve(auction.address, ethers.BigNumber.from(0))
